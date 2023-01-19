48e04d20665cd56fb15d60141721e9f963d255d8
Nicolas Laval <nicolas.laval@making-sense.info>
2023-01-19 12:17:12 +0100

    Fix Trevas local Kernel configuration (#22)
    * Fix Trevas local Kernel configuration
    
    * Update Dockerfile
    
    * Update Hadoop config
    
    * Update SparkUtils.java
    
    * Delete local Kernel
    
    * Revert "Delete local Kernel"
    
    This reverts commit b6a48e971f6be9db399b63bd09e7f5da5444e953.

--- src/main/java/fr/insee/trevas/jupyter/SparkUtils.java
+++ src/main/java/fr/insee/trevas/jupyter/SparkUtils.java
@@ -33,10 +33,12 @@ public class SparkUtils {
                 "/vtl-spark.jar",
                 "/vtl-model.jar",
                 "/vtl-parser.jar",
-                "/vtl-engine.jar"
+                "/vtl-engine.jar",
+                "/hadoop-aws.jar"
         ));
         conf.set("spark.kubernetes.container.image", "inseefrlab/spark-hadoop:trevas-0.4.8-spark-3.2.1-hadoop-3.3.1-postgresql-42.3.3-postgis-2021.1.0");
         conf.set("spark.kubernetes.container.pullPolicy", "IfNotPresent");
+        conf.set("spark.hadoop.fs.s3a.bucket.all.committer.magic.enabled", "false");
         return sparkBuilder.config(conf).getOrCreate();
     }
 
