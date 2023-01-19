3e8152d432ea6b6a6e7cf8c843d0d22311f9b353
NicoLaval <nicolas.laval@making-sense.info>
2023-01-19 11:39:57 +0100

    Update SparkUtils.java

--- src/main/java/fr/insee/trevas/jupyter/SparkUtils.java
+++ src/main/java/fr/insee/trevas/jupyter/SparkUtils.java
@@ -38,6 +38,7 @@ public class SparkUtils {
         ));
         conf.set("spark.kubernetes.container.image", "inseefrlab/spark-hadoop:trevas-0.4.8-spark-3.2.1-hadoop-3.3.1-postgresql-42.3.3-postgis-2021.1.0");
         conf.set("spark.kubernetes.container.pullPolicy", "IfNotPresent");
+        conf.set("spark.hadoop.fs.s3a.bucket.all.committer.magic.enabled", "false");
         return sparkBuilder.config(conf).getOrCreate();
     }
 
