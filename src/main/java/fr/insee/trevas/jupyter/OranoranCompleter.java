7b2a1d8f3f526de628897e056dae78b42f2971e4
NicoLaval <nicolas.laval@making-sense.info>
2023-08-11 10:19:10 +0200

    Refine autocomplete tests

---
+++ src/main/java/fr/insee/trevas/jupyter/OranoranCompleter.java
@@ -0,0 +1,29 @@
+package fr.insee.trevas.jupyter;
+
+import com.intigua.antlr4.autosuggest.AutoSuggester;
+import com.intigua.antlr4.autosuggest.LexerAndParserFactory;
+import com.intigua.antlr4.autosuggest.ReflectionLexerAndParserFactory;
+import fr.insee.vtl.parser.VtlLexer;
+import fr.insee.vtl.parser.VtlParser;
+import io.github.spencerpark.jupyter.kernel.ReplacementOptions;
+
+import javax.script.Bindings;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class OranoranCompleter implements AutoCompleter {
+
+    private final static LexerAndParserFactory lexerAndParserFactory =
+            new ReflectionLexerAndParserFactory(
+                    VtlLexer.class, VtlParser.class
+            );
+
+    public OranoranCompleter(Bindings bindings) {
+    }
+
+    @Override
+    public ReplacementOptions complete(String code, int at) {
+        Collection<String> suggestions = new AutoSuggester(lexerAndParserFactory, code).suggestCompletions();
+        return new ReplacementOptions(new ArrayList<>(suggestions), at, at);
+    }
+}
