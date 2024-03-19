8a4e30d0c9e9b1eb29e40b30b71655cc6e7c35cf
Nicolas Laval <nicolas.laval@making-sense.info>
2024-03-19 23:26:17 +0100

    Improve CSV tests

--- src/test/java/CSVTest.java
+++ src/test/java/CSVTest.java
@@ -40,7 +40,7 @@ public class CSVTest {
                 SparkSession.builder().appName("test").master("local").getOrCreate()) {
             Dataset ds1 = SparkUtils.readCSVDataset(spark, "src/test/resources/ds1.csv");
             assertThat(ds1.getDataPoints().get(1).get("name")).isEqualTo("B");
-            Dataset ds2 = SparkUtils.readCSVDataset(spark, "src/test/resources/ds2.csv?sep=%7C");
+            Dataset ds2 = SparkUtils.readCSVDataset(spark, "src/test/resources/ds2.csv?delimiter=%7C&quote=%27");
             assertThat(ds2.getDataPoints().get(1).get("name")).isEqualTo("G");
         }
     }
