43fbf90ecbcb2e7cff708f69aa422ed4e50e4e08
Nicolas Laval <nicolas.laval@making-sense.info>
2024-07-03 08:35:52 +0200

    Remove ratchetFrom from spotless configuration

--- src/main/java/fr/insee/trevas/jupyter/DatasetUtils.java
+++ src/main/java/fr/insee/trevas/jupyter/DatasetUtils.java
@@ -6,77 +6,77 @@ import fr.insee.vtl.model.Structured;
 
 public class DatasetUtils {
 
-    public static String datasetToDisplay(Dataset dataset) {
-        var b = new StringBuilder();
-        b.append("<table id='dataset_").append(dataset.hashCode()).append("' class='display'>");
-        b.append("<thead>");
-        b.append("<tr>");
-        dataset.getDataStructure()
-                .forEach(
-                        (name, component) -> {
-                            b.append("<th>").append(name).append("</th>");
-                        });
-        b.append("</tr>");
-        b.append("</thead>");
-        b.append("<tbody>");
-        dataset.getDataPoints()
-                .forEach(
-                        row -> {
-                            b.append("<tr>");
-                            dataset.getDataStructure()
-                                    .keySet()
-                                    .forEach(
-                                            name -> {
-                                                b.append("<td>")
-                                                        .append(row.get(name))
-                                                        .append("</td>");
-                                            });
-                            b.append("</tr>");
-                        });
-        b.append("</tbody>");
-        b.append("</table>");
-        b.append(
-                "<script\n"
-                        + "  src=\"https://code.jquery.com/jquery-3.6.0.slim.min.js\"\n"
-                        + "  integrity=\"sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=\"\n"
-                        + "  crossorigin=\"anonymous\"></script>");
-        b.append(
-                "<link rel=\"stylesheet\" type=\"text/css\""
-                    + " href=\"https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css\">\n"
-                    + "  \n"
-                    + "<script type=\"text/javascript\" charset=\"utf8\""
-                    + " src=\"https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js\"></script>\n");
-        b.append(
-                "<script type=\"text/javascript\">"
-                        + "$(document).ready( function () {\n"
-                        + "    $('#dataset_"
-                        + dataset.hashCode()
-                        + "').DataTable();\n"
-                        + "} );"
-                        + "</script>");
+	public static String datasetToDisplay(Dataset dataset) {
+		var b = new StringBuilder();
+		b.append("<table id='dataset_").append(dataset.hashCode()).append("' class='display'>");
+		b.append("<thead>");
+		b.append("<tr>");
+		dataset.getDataStructure()
+				.forEach(
+						(name, component) -> {
+							b.append("<th>").append(name).append("</th>");
+						});
+		b.append("</tr>");
+		b.append("</thead>");
+		b.append("<tbody>");
+		dataset.getDataPoints()
+				.forEach(
+						row -> {
+							b.append("<tr>");
+							dataset.getDataStructure()
+									.keySet()
+									.forEach(
+											name -> {
+												b.append("<td>")
+														.append(row.get(name))
+														.append("</td>");
+											});
+							b.append("</tr>");
+						});
+		b.append("</tbody>");
+		b.append("</table>");
+		b.append(
+				"<script\n"
+						+ "  src=\"https://code.jquery.com/jquery-3.6.0.slim.min.js\"\n"
+						+ "  integrity=\"sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=\"\n"
+						+ "  crossorigin=\"anonymous\"></script>");
+		b.append(
+				"<link rel=\"stylesheet\" type=\"text/css\""
+					+ " href=\"https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css\">\n"
+					+ "  \n"
+					+ "<script type=\"text/javascript\" charset=\"utf8\""
+					+ " src=\"https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js\"></script>\n");
+		b.append(
+				"<script type=\"text/javascript\">"
+						+ "$(document).ready( function () {\n"
+						+ "    $('#dataset_"
+						+ dataset.hashCode()
+						+ "').DataTable();\n"
+						+ "} );"
+						+ "</script>");
 
-        return b.toString();
-    }
+		return b.toString();
+	}
 
-    public static String datasetMetadataToDisplay(Dataset dataset) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("<ul>");
-        Structured.DataStructure dataStructure = dataset.getDataStructure();
-        dataStructure.forEach(
-                (key, value) -> {
-                    sb.append("<li>")
-                            .append(key)
-                            .append(" (")
-                            .append(value.getRole().name())
-                            .append(" - ")
-                            .append(value.getType().getSimpleName());
-                    String valuedomain = value.getValuedomain();
-                    if (null != valuedomain) {
-                        sb.append(" - ").append(valuedomain);
-                    }
-                    sb.append(")").append("</li>").append("\n");
-                });
-        sb.append("</ul>");
-        return sb.toString();
-    }
+	public static String datasetMetadataToDisplay(Dataset dataset) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<ul>");
+		Structured.DataStructure dataStructure = dataset.getDataStructure();
+		dataStructure.forEach(
+				(key, value) -> {
+					sb.append("<li>")
+							.append(key)
+							.append(" (")
+							.append(value.getRole().name())
+							.append(" - ")
+							.append(value.getType().getSimpleName());
+					String valuedomain = value.getValuedomain();
+					if (null != valuedomain) {
+						sb.append(" - ").append(valuedomain);
+					}
+					sb.append(")").append("</li>").append("\n");
+				});
+		sb.append("</ul>");
+		return sb.toString();
+	}
 }
--- src/main/java/fr/insee/trevas/jupyter/SparkUtils.java
+++ src/main/java/fr/insee/trevas/jupyter/SparkUtils.java
@@ -15,93 +15,93 @@ import org.apache.spark.sql.SparkSession;
 
 public class SparkUtils {
 
-    public static VtlScriptEngine buildSparkEngine(SparkSession spark) {
-        ScriptEngineManager mgr = new ScriptEngineManager();
-        ScriptEngine engine = mgr.getEngineByExtension("vtl");
-        engine.put("$vtl.engine.processing_engine_names", "spark");
-        engine.put("$vtl.spark.session", spark);
-        return (VtlScriptEngine) engine;
-    }
+	public static VtlScriptEngine buildSparkEngine(SparkSession spark) {
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		ScriptEngine engine = mgr.getEngineByExtension("vtl");
+		engine.put("$vtl.engine.processing_engine_names", "spark");
+		engine.put("$vtl.spark.session", spark);
+		return (VtlScriptEngine) engine;
+	}
 
-    public static SparkSession buildSparkSession() {
-        SparkSession.Builder sparkBuilder = SparkSession.builder();
-        SparkConf conf = new SparkConf(true);
-        String spark_home = System.getenv("SPARK_HOME") + "/conf";
-        Path path = Path.of(spark_home, "spark-defaults.conf");
-        if (Files.exists(path)) {
-            org.apache.spark.util.Utils.loadDefaultSparkProperties(
-                    conf, path.normalize().toAbsolutePath().toString());
-            if (conf.get("spark.jars", "").isEmpty()) {
-                conf.set(
-                        "spark.jars",
-                        String.join(
-                                ",",
-                                "/vtl-spark.jar",
-                                "/vtl-model.jar",
-                                "/vtl-parser.jar",
-                                "/vtl-engine.jar"));
-            }
-        } else {
-            sparkBuilder.master("local");
-        }
-        return sparkBuilder.config(conf).getOrCreate();
-    }
+	public static SparkSession buildSparkSession() {
+		SparkSession.Builder sparkBuilder = SparkSession.builder();
+		SparkConf conf = new SparkConf(true);
+		String spark_home = System.getenv("SPARK_HOME") + "/conf";
+		Path path = Path.of(spark_home, "spark-defaults.conf");
+		if (Files.exists(path)) {
+			org.apache.spark.util.Utils.loadDefaultSparkProperties(
+					conf, path.normalize().toAbsolutePath().toString());
+			if (conf.get("spark.jars", "").isEmpty()) {
+				conf.set(
+						"spark.jars",
+						String.join(
+								",",
+								"/vtl-spark.jar",
+								"/vtl-model.jar",
+								"/vtl-parser.jar",
+								"/vtl-engine.jar"));
+			}
+		} else {
+			sparkBuilder.master("local");
+		}
+		return sparkBuilder.config(conf).getOrCreate();
+	}
 
-    public static SparkDataset readParquetDataset(SparkSession spark, String path)
-            throws Exception {
-        Dataset<Row> dataset;
-        try {
-            dataset = spark.read().parquet(path);
-        } catch (Exception e) {
-            throw new Exception("Bad format", e);
-        }
-        return new SparkDataset(dataset);
-    }
+	public static SparkDataset readParquetDataset(SparkSession spark, String path)
+			throws Exception {
+		Dataset<Row> dataset;
+		try {
+			dataset = spark.read().parquet(path);
+		} catch (Exception e) {
+			throw new Exception("Bad format", e);
+		}
+		return new SparkDataset(dataset);
+	}
 
-    public static SparkDataset readSasDataset(SparkSession spark, String path) throws Exception {
-        Dataset<Row> dataset;
-        try {
-            dataset = spark.read().format("com.github.saurfang.sas.spark").load(path);
-        } catch (Exception e) {
-            throw new Exception(e);
-        }
-        return new SparkDataset(dataset);
-    }
+	public static SparkDataset readSasDataset(SparkSession spark, String path) throws Exception {
+		Dataset<Row> dataset;
+		try {
+			dataset = spark.read().format("com.github.saurfang.sas.spark").load(path);
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+		return new SparkDataset(dataset);
+	}
 
-    public static SparkDataset readCSVDataset(SparkSession spark, String path) throws Exception {
-        Dataset<Row> dataset;
-        var uri = Utils.uri(path);
-        var params = new Utils.QueryParam(uri);
-        try {
-            dataset =
-                    spark.read()
-                            .option("delimiter", params.getValue("delimiter").orElse(";"))
-                            .option("quote", params.getValue("quote").orElse("\""))
-                            .option("header", params.getValue("header").orElse("true"))
-                            .options(params.flatten())
-                            .csv(Path.of(Utils.strip(uri)).normalize().toAbsolutePath().toString());
-        } catch (Exception e) {
-            throw new Exception(e);
-        }
-        return new SparkDataset(dataset);
-    }
+	public static SparkDataset readCSVDataset(SparkSession spark, String path) throws Exception {
+		Dataset<Row> dataset;
+		var uri = Utils.uri(path);
+		var params = new Utils.QueryParam(uri);
+		try {
+			dataset =
+					spark.read()
+							.option("delimiter", params.getValue("delimiter").orElse(";"))
+							.option("quote", params.getValue("quote").orElse("\""))
+							.option("header", params.getValue("header").orElse("true"))
+							.options(params.flatten())
+							.csv(Path.of(Utils.strip(uri)).normalize().toAbsolutePath().toString());
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+		return new SparkDataset(dataset);
+	}
 
-    public static void writeParquetDataset(String location, SparkDataset dataset) {
-        org.apache.spark.sql.Dataset<Row> sparkDataset = dataset.getSparkDataset();
-        sparkDataset.write().mode(SaveMode.Overwrite).parquet(location);
-    }
+	public static void writeParquetDataset(String location, SparkDataset dataset) {
+		org.apache.spark.sql.Dataset<Row> sparkDataset = dataset.getSparkDataset();
+		sparkDataset.write().mode(SaveMode.Overwrite).parquet(location);
+	}
 
-    public static void writeCSVDataset(String location, SparkDataset dataset) {
-        org.apache.spark.sql.Dataset<Row> sparkDataset = dataset.getSparkDataset();
-        var uri = Utils.uri(location);
-        var params = new Utils.QueryParam(uri);
-        sparkDataset
-                .write()
-                .option("delimiter", params.getValue("delimiter").orElse(";"))
-                .option("quote", params.getValue("quote").orElse("\""))
-                .option("header", params.getValue("header").orElse("true"))
-                .options(params.flatten())
-                .mode(SaveMode.Overwrite)
-                .csv(Path.of(Utils.strip(uri)).normalize().toAbsolutePath().toString());
-    }
+	public static void writeCSVDataset(String location, SparkDataset dataset) {
+		org.apache.spark.sql.Dataset<Row> sparkDataset = dataset.getSparkDataset();
+		var uri = Utils.uri(location);
+		var params = new Utils.QueryParam(uri);
+		sparkDataset
+				.write()
+				.option("delimiter", params.getValue("delimiter").orElse(";"))
+				.option("quote", params.getValue("quote").orElse("\""))
+				.option("header", params.getValue("header").orElse("true"))
+				.options(params.flatten())
+				.mode(SaveMode.Overwrite)
+				.csv(Path.of(Utils.strip(uri)).normalize().toAbsolutePath().toString());
+	}
 }
