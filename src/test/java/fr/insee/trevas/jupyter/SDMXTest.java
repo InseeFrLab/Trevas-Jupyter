bffca3f4deb7ce894d2d87e08d0eb8ce7378d747
Nicolas Laval <nicolas.laval@making-sense.info>
2024-07-03 08:24:37 +0200

    Manually run spotless

--- src/test/java/fr/insee/trevas/jupyter/SDMXTest.java
+++ src/test/java/fr/insee/trevas/jupyter/SDMXTest.java
@@ -9,52 +9,52 @@ import org.junit.jupiter.api.Test;
 
 public class SDMXTest {
 
-    @Test
-    public void testLoadSDMXSource() {
-        Dataset ds =
-                VtlKernel.loadSDMXEmptySource(
-                        "src/test/resources/sdmx/DSD_BPE_CENSUS.xml", "BPE_DETAIL_VTL");
-        assertThat(ds.getDataStructure().size()).isEqualTo(6);
-    }
+	@Test
+	public void testLoadSDMXSource() {
+		Dataset ds =
+				VtlKernel.loadSDMXEmptySource(
+						"src/test/resources/sdmx/DSD_BPE_CENSUS.xml", "BPE_DETAIL_VTL");
+		assertThat(ds.getDataStructure().size()).isEqualTo(6);
+	}
 
-    @Test
-    public void testLoadSDMXSourceWithData() throws Exception {
-        new VtlKernel();
-        Dataset ds =
-                VtlKernel.loadSDMXSource(
-                        "src/test/resources/sdmx/DSD_BPE_CENSUS.xml",
-                        "BPE_DETAIL_VTL",
-                        "src/test/resources/sdmx/BPE_DETAIL_SAMPLE.csv");
-        assertThat(ds.getDataStructure().size()).isEqualTo(6);
-    }
+	@Test
+	public void testLoadSDMXSourceWithData() throws Exception {
+		new VtlKernel();
+		Dataset ds =
+				VtlKernel.loadSDMXSource(
+						"src/test/resources/sdmx/DSD_BPE_CENSUS.xml",
+						"BPE_DETAIL_VTL",
+						"src/test/resources/sdmx/BPE_DETAIL_SAMPLE.csv");
+		assertThat(ds.getDataStructure().size()).isEqualTo(6);
+	}
 
-    @Test
-    public void testRunSDMXPreview() throws Exception {
-        new VtlKernel();
-        VtlKernel.runSDMXPreview("src/test/resources/sdmx/DSD_BPE_CENSUS.xml");
-    }
+	@Test
+	public void testRunSDMXPreview() throws Exception {
+		new VtlKernel();
+		VtlKernel.runSDMXPreview("src/test/resources/sdmx/DSD_BPE_CENSUS.xml");
+	}
 
-    @Test
-    public void testRunSDMX() throws Exception {
-        new VtlKernel();
-        VtlKernel.runSDMX(
-                "src/test/resources/sdmx/DSD_BPE_CENSUS.xml",
-                Map.of(
-                        "BPE_DETAIL_VTL",
-                        "src/test/resources/sdmx/BPE_DETAIL_SAMPLE.csv",
-                        "LEGAL_POP",
-                        "src/test/resources/sdmx/LEGAL_POP_NUTS3.csv"));
-    }
+	@Test
+	public void testRunSDMX() throws Exception {
+		new VtlKernel();
+		VtlKernel.runSDMX(
+				"src/test/resources/sdmx/DSD_BPE_CENSUS.xml",
+				Map.of(
+						"BPE_DETAIL_VTL",
+						"src/test/resources/sdmx/BPE_DETAIL_SAMPLE.csv",
+						"LEGAL_POP",
+						"src/test/resources/sdmx/LEGAL_POP_NUTS3.csv"));
+	}
 
-    @Test
-    public void testGetTransformationsVTL() throws Exception {
-        new VtlKernel();
-        VtlKernel.getTransformationsVTL("src/test/resources/sdmx/DSD_BPE_CENSUS.xml");
-    }
+	@Test
+	public void testGetTransformationsVTL() throws Exception {
+		new VtlKernel();
+		VtlKernel.getTransformationsVTL("src/test/resources/sdmx/DSD_BPE_CENSUS.xml");
+	}
 
-    @Test
-    public void testGetRulesetsVTL() throws Exception {
-        new VtlKernel();
-        VtlKernel.getRulesetsVTL("src/test/resources/sdmx/DSD_BPE_CENSUS.xml");
-    }
+	@Test
+	public void testGetRulesetsVTL() throws Exception {
+		new VtlKernel();
+		VtlKernel.getRulesetsVTL("src/test/resources/sdmx/DSD_BPE_CENSUS.xml");
+	}
 }
